4ba4feab84117d45262f7e9fc6092258421eb1b0
cmridene <cmridene@iu.edu>
2019-05-29 13:12:53 -0400

    Unwarranted Master branch submission

--- Survey Page/Scripts/Survey.js
+++ Survey Page/Scripts/Survey.js
@@ -2,8 +2,8 @@ function WriteToFile(passForm){
   alert("submitted")
   set fso = CreateObject("Scripting.FileSystemObject");
   set s = fso.CreateTextFile("filename.txt", True);
-  var firstName = document.getElementById('First Name')
-  var lastName = document.getElementById('Last Name')
+  var firstName = document.getElementById('firstname')
+  var lastName = document.getElementById('lastname')
   s.writeline("First Name:" + firstName);
   s.writeline("Last Name: ") + lastName);
   s.close();
